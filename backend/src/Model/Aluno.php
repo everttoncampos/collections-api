22776fb1b630e1e524eb06f223a62007c0f506a9
Evertton Matheus <evertton.g.l@hotmail.com>
2020-12-29 18:12:11 -0400

    Adicionada função que retorna todos os alunos adicionados.

--- backend/src/Model/Aluno.php
+++ backend/src/Model/Aluno.php
@@ -1,7 +1,58 @@
 <?php
 
-namespace model;
+namespace src\Model;
+
+use src\Core\Database;
 
 class Aluno {
+
+  private static $array;
+  private static $con;
+  private static $db;
+
+  public function __construct() {
+
+    self::$con = new Database();
+    self::$db = self::$con->connect();
+
+  }
+
+  public function cadastrarAluno($nome, $sobrenome, $cpf, $turma) {
+    
+    $sql = self::$db->prepare('INSERT INTO aluno (nome, sobrenome, cpf, turma_id) VALUES (:nome, :sobrenome, :cpf, :turma_id)');
+    $sql->bindValue(':nome', $nome);
+    $sql->bindValue(':sobrenome', $sobrenome);
+    $sql->bindValue(':cpf', $cpf);
+    $sql->bindValue(':turma_id', $turma);
+    $sql->execute();
+
+    if ($sql) {
+      return true;
+    }
+
+  }
+
+  public function getAlunosProfessor($idTurma) {
+
+    $sql = self::$db->prepare('SELECT * FROM aluno WHERE turma_id = :id');
+    $sql->bindValue(':id', $idTurma);
+    $sql->execute();
+
+    if ($sql->rowCount() > 0) {
+      $dados = $sql->fetchAll(\PDO::FETCH_ASSOC);
+      return $dados;
+    }
+
+  }
+
+  public function getAlunos() {
+    $sql = self::$db->prepare('SELECT * FROM aluno');
+    $sql->execute();
+
+    if ($sql->rowCount() > 0){
+      $dados = $sql->fetchAll(\PDO::FETCH_ASSOC);
+      return $dados;
+    }
+  }
 
 }
