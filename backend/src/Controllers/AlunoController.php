8334509247ae58a9f0a03d04a65f0ca021cbe788
Evertton Matheus <evertton.g.l@hotmail.com>
2021-01-03 10:52:10 -0400

    Adicionado funções que auxiliaram no restorno das respostas

--- backend/src/Controllers/AlunoController.php
+++ backend/src/Controllers/AlunoController.php
@@ -12,6 +12,14 @@ class AlunoController {
     self::$aluno = new Aluno();
   }
 
+  public function verificaAluno($cpf){
+    $aluno = self::$aluno->verificaAluno($cpf);
+
+    $array['success'] = true;
+    $array['result']['User'] = $aluno;
+    return $array;
+  }
+
   public function cadastrarAluno ($nome, $sobrenome, $cpf, $turma) {
     $val = self::$aluno->cadastrarAluno($nome, $sobrenome, $cpf, $turma);
 
@@ -25,6 +33,22 @@ class AlunoController {
   public function getAlunos() {
     $alunos = self::$aluno->getAlunos();
     return $alunos;
+  }
+
+  public function getAlunosProfessor($id) {
+    $alunos = self::$aluno->getAlunosProfessor($id);
+    // echo json_encode($alunos);
+    return $alunos;
+  }
+
+  public function excluirAluno($id) {
+    try {
+      self::$aluno->excluirAluno($id);
+
+    } catch (PDOException $e) {
+      echo "Error: ".$e->getMessage();
+      
+    }
   }
 
 
